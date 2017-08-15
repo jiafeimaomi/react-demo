f23c00fcf306ac374d22d5517461a2b0b89ad4c1
liufang <liufang@suneee.com>
2017-08-15 13:51:18 +0800

    提交gitignore文件

--- src/App.js
+++ src/App.js
@@ -5,17 +5,17 @@ import Button from 'antd-mobile/lib/button/index.web';
 
 class App extends Component {
   render() {
-return (
-<div className="App">
-<div className="App-header">
-  <img src={logo} className="App-logo" alt="logo" />
-  <h2>Welcome to React</h2>
-</div>
-<p className="App-intro">
-  <Button>按钮</Button>
-</p>
-</div>
-);
+    return (
+      <div className="App">
+      <div className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h2>Welcome to React</h2>
+      </div>
+      <p className="App-intro">
+        <Button>按钮</Button>
+      </p>
+      </div>
+    );
   }
 }
 
