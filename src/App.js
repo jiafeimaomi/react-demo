cd1a31b213a8c3ce813af19f563db8e8b15d7e48
liufang <liufang@suneee.com>
2017-08-15 13:27:00 +0800

    create-react-app

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import logo from './static/images/logo.svg';
+import './static/style/App.css';
+import Button from 'antd-mobile/lib/button/index.web';
+
+class App extends Component {
+  render() {
+return (
+<div className="App">
+<div className="App-header">
+  <img src={logo} className="App-logo" alt="logo" />
+  <h2>Welcome to React</h2>
+</div>
+<p className="App-intro">
+  <Button>按钮</Button>
+</p>
+</div>
+);
+  }
+}
+
+export default App;
